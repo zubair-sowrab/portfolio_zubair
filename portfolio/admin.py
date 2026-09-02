73d5a0273597a839afe256905f758ce5b38df488
mynuaimi46 <mynuaimi46@gmail.com>
2026-09-02 17:20:02 +0400

    companies

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SiteSetting, Experience, Education, Project, Certification, Skill, ExtraCurricular, Reference
+from .models import SiteSetting, Experience,Company, Education, Project, Certification, Skill, ExtraCurricular, Reference
 
 admin.site.register(SiteSetting)
 admin.site.register(Experience)
@@ -9,3 +9,4 @@ admin.site.register(Certification)
 admin.site.register(Skill)
 admin.site.register(ExtraCurricular)
 admin.site.register(Reference)
+admin.site.register(Company)
